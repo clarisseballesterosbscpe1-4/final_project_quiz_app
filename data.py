9a8c0dd452c6ab16c54de3bbb8995cd6b6c42d77
clang4py <clarisseballesteros08@gmail.com>
2026-06-18 18:20:00 +0800

    origin data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {
+        "text": "Python is a programming language?",
+        "answer": "True"
+    },
+    {
+        "text": "HTML is a programming language?",
+        "answer": "False"
+    },
+    {
+        "text": "OOP means Object Oriented Programming?",
+        "answer": "True"
+    }
+]
