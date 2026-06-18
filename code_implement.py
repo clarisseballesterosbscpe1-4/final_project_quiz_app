cc6cc58e0df3196ef92323ad7192aaf69f467a37
clang4py <clarisseballesteros08@gmail.com>
2026-06-19 03:36:00 +0800

    implement polymorphism

--- code_implement.py
+++ code_implement.py
@@ -12,3 +12,20 @@ class Player(User):
     def __init__(self, name):
         super().__init__(name)
         self.score = 0
+
+# POLYMORPHISM
+
+class Admin:
+
+    def display(self):
+        print("Admin User")
+
+class QuizPlayer:
+
+    def display(self):
+        print("Quiz Player")
+
+users = [Admin(),QuizPlayer()]
+
+for user in users:
+    user.display()
